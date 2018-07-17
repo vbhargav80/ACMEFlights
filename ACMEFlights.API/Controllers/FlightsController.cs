4a55706b1868f52bd0c8b0eba10e218a3df71eb7
vbhargav80 <varun.bhargava80@gmail.com>
2018-07-17 22:02:48 +1000

    removed unnecessary empty api methods

--- ACMEFlights.API/Controllers/FlightsController.cs
+++ ACMEFlights.API/Controllers/FlightsController.cs
@@ -23,20 +23,5 @@ namespace ACMEFlights.API.Controllers
         {
             return _availabilityService.GetAvailability(searchRequest);
         }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
